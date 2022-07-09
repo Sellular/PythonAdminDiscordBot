df0df3adecc409113c4b3234470965416ffe61fc
Sellular <sellular13@gmail.com>
2022-07-09 16:41:03 +0000

    Added id to object

--- model/AdminEvent.py
+++ model/AdminEvent.py
@@ -1,12 +1,14 @@
 class AdminEvent:
 
+    id = 0
     userID = ''
     guildID = ''
     eventCode = ''
     eventReason = None
     eventTimestamp = ''
 
-    def __init__(self, userID, guildID, eventCode, eventReason, eventTimestamp):
+    def __init__(self, id, userID, guildID, eventCode, eventReason, eventTimestamp):
+        self.id = id
         self.userID = userID
         self.guildID = guildID
         self.eventCode = eventCode
